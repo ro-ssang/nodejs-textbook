8122fd53b8b4435f14aa0873b63167a91c50cae2
rossa <fashionfish@naver.com>
2021-06-18 19:15:50 +0900

    4장 fs로 HTML 읽어 제공하기

--- ch04/4.1/server1.js
+++ ch04/4.1/server1.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http
     .createServer((req, res) => {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" }); // HTML 코드를 일반 문자로 읽을 수 있으므로 res 헤더에 "text/html"이라고 설정한다.
         res.write("<h1>Hello Node</h1>");
         res.end("<p>Hello Server!</p>");
     })
