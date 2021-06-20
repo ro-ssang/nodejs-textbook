536efdb7318addae33379bf9cbd5fd3f86182495
rossa <fashionfish@naver.com>
2021-06-20 22:09:09 +0900

    6장 next 활용법

--- ch06/6.2/app.js
+++ ch06/6.2/app.js
@@ -7,11 +7,28 @@ app.set("port", process.env.PORT || 3000);
 
 // 에러 발생 시키기
 app.use((req, res, next) => {
-    throw new Error("에러 발생!");
+    try {
+        console.log(asdgasdg);
+    } catch (error) {
+        next(error); // next()의 인수에 값을 할당하면 에러로 처리된다.
+    }
 });
 
+app.get(
+    "/",
+    (req, res, next) => {
+        if (true) {
+            next("route"); // next()의 인수에 "route"를 넣으면 다음 미들웨어는 실행 되지 않고, 일치하는 다음 라우터가 실행된다(여기서는 "/").
+        }
+        res.sendFile(path.join(__dirname, "index.html"));
+    },
+    (req, res) => {
+        console.log("실행 되나요?");
+    }
+);
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    console.log("실행 됩니다.");
 });
 
 app.get("/about", (req, res) => {
