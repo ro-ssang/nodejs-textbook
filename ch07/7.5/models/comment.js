0e0198952a588cc21b8080d8143a42eb1950d31b
rossa <fashionfish@naver.com>
2021-06-23 20:29:47 +0900

    7장 테이블 관계 이해하기

--- ch07/7.5/models/comment.js
+++ ch07/7.5/models/comment.js
@@ -2,26 +2,34 @@ const Sequelize = require("sequelize");
 
 module.exports = class User extends Sequelize.Model {
     static init(sequelize) {
-        return super.init({
-            comment: {
-                type: Sequelize.STRING(100),
-                allowNull: false
+        return super.init(
+            {
+                comment: {
+                    type: Sequelize.STRING(100),
+                    allowNull: false,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.NOW,
+                },
             },
-            created_at: {
-                type: Sequelize.DATE,
-                allowNull: false,
-                defaultValue: Sequelize.NOW,
+            {
+                sequelize,
+                timestamps: false,
+                underscored: false,
+                modelName: "Comment",
+                tableName: "comments",
+                paranoid: false,
+                charset: "utf8",
+                collate: "utf8_general_ci",
             }
-        }, {
-            sequelize,
-            timestamps: false,
-            underscored: false,
-            modelName: "Comment",
-            tableName: "comments",
-            paranoid: false,
-            charset: "utf8",
-            collate: "utf8_general_ci",
-        })
+        );
     }
-    static associate(db) {}
-}
+    static associate(db) {
+        db.Comment.belongsTo(db.User, {
+            foreignKey: "commenter",
+            targetKey: "id",
+        });
+    }
+};
